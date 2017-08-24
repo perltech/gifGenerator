576baad4f81e730299550f4440bb94ca0595ac7c
perltech <noah.perlmutter@gmail.com>
2017-08-24 11:54:41 -0600

    display gifs for initially loaded buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,14 +6,12 @@ function renderButtons() {
 	$("#button-generator").html("<div></div>");
 
 	$.each(topics, function(index, topic) {
-
 		var btn = $("<button>");
 		btn.append(topic);
+		btn.addClass("gif-click")
 		btn.attr("gif-name", topic);
-		$("#button-generator").append(btn);
-		
+		$("#button-generator").append(btn);	
 	});
-
 };
 
 $("#gif-button").click( function(event) {
@@ -30,8 +28,35 @@ $("#gif-button").click( function(event) {
 renderButtons();
 
 
-$("#gif-button").click(function(){
+$(".gif-click").click(function(){
+	var image = $(this).attr("gif-name");
 
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        image + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        .done(function(response) {
+          var results = response.data;
+
+          for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item'>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var personImage = $("<img>");
+            personImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+            console.log(results);
+            $("#gifs").prepend(gifDiv);
+          }
+        });
 });
 
 
