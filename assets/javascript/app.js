bb1fbfe1a54c7fee3616a19e15aaef3cc2c0335e
perltech <noah.perlmutter@gmail.com>
2017-08-24 11:33:19 -0600

    Generate IDs for buttons dynamically

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,25 +3,37 @@ $( document ).ready(function() {
 var topics = ["Erykah Badu", "Man or Astroman", "Pink Floyd", "Jurassic 5"];
 
 function renderButtons() {
-	$("#gifs").html("<div></div>");
+	$("#button-generator").html("<div></div>");
 
 	$.each(topics, function(index, topic) {
-		$("#gifs").append("<button>" + topic + "</button>");
+
+		var btn = $("<button>");
+		btn.append(topic);
+		btn.attr("gif-name", topic);
+		$("#button-generator").append(btn);
+		
 	});
 
 };
 
-	$("#gif-button").click( function(event) {
-		event.preventDefault();
-		var userInput = $("#gif-text").val();
-		topics.push(userInput);
-		console.log(topics);
+$("#gif-button").click( function(event) {
+	event.preventDefault();
+	
+	var userInput = $("#gif-text").val();
+	topics.push(userInput);
+	console.log(topics);
 
-		renderButtons();
+	renderButtons();
 
-	});
+});
 
 renderButtons();
+
+
+$("#gif-button").click(function(){
+
+});
+
 
 
 
