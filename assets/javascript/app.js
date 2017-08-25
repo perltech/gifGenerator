ab17ae575e3e672c5a532605f2b95a8e4c6b4532
perltech <noah.perlmutter@gmail.com>
2017-08-25 11:30:51 -0600

    add http secure to api call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ renderButtons();
 $('#button-generator').on('click', '.gif-click', function() {
 	var image = $(this).attr("gif-name");
 
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         image + "&api_key=dc6zaTOxFJmzC&limit=10";
 
       $.ajax({
